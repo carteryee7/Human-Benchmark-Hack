700dfaa8f921f1de79dcb4ca7f2800574860c54d
Carter Yee <carter.yee07@gmail.com>
2025-12-30 00:19:28 -0500

    Add files via upload

---
+++ aimbot.py
@@ -0,0 +1,35 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+#Color of center: (255, 219, 195)
+
+while keyboard.is_pressed('q') == False:
+    flag = 0
+    pic = pyautogui.screenshot(region=(858, 199, 830, 448))
+
+    width, height = pic.size
+
+    for x in range(0, width, 5):
+        for y in range(0, height, 5):
+
+            r, g, b = pic.getpixel((x, y))
+
+            if b == 232 and r == 149 and g == 195:
+                flag = 1
+                click(x+858, y+199)
+                time.sleep(0.05)
+                break
+
+        if flag == 1:
+            break
